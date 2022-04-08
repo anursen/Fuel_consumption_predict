b4eca33ed950bbfe6cb2412302dea425e0888b11
serhat <sguven1@yandex.com>
2022-04-07 23:30:04 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def calculate_mpg(actual_mpg,velocity):
+  mpg = (-0.00062 * actual_mpg**2 + 0.01345*actual_mpg - 0.34192) * velocity + 0.0533*actual_mpg**2 - 0.3580* actual_mpg + 30.0424 
+  return mpg
+
+def calculate_cost_of_travel_time(hourly_rate,distance,mph):
+  travel_time = distance/mph
+  return hourly_rate * travel_time
+
+
+def calc_gas_cost(total_distance,mpg,gas_price=4,):
+  return total_distance / mpg * gas_price
+
+
+Distance_to_work = st.slider('distance to work',0, 100, 1)
+Gas_Price = st.slider('gas price',0.0,10.0, 4.1)
+Hourly_rate = st.slider('hourly rate', 0, 100, 13)
+Cars_mpg = st.slider('mpg', 5, 100, 30)
+
+speed_series = np.linspace(55,90,50)
+mpg_series = calculate_mpg(Cars_mpg,speed_series)
+cost_series = calculate_cost_of_travel_time(Hourly_rate,Distance_to_work,speed_series) + calc_gas_cost(Distance_to_work,mpg_series)
+cost_of_time_series = calculate_cost_of_travel_time(Hourly_rate,Distance_to_work,mpg_series)
+
+result = round(float(speed_series[np.where(cost_series == cost_series.min())]),2)
+st.text(result)
